727deff962aa3079ac86816544d16a76c8e8387e
Aaron3007-wq <22amclynn.acc@lwetb.ie>
2025-09-02 13:48:51 +0100

    Add files via upload

---
+++ Programming 2.py
@@ -0,0 +1,12 @@
+#Author:Aaron Mclynn
+#Date: 01 Setember 1
+#Description:Program to request user name as input and return same with a greeting
+name = input('What is your name')
+surname = input('What is your surname')
+print('Hello',name,surname)
+number1 = input('enter 1st number')
+number1 = int (number1)
+number2 = input('enter 2nd number')
+number2 = int (number2)
+total = number1 + number2
+print(total)
