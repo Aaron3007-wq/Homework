fac4bdf84a39ef28a63aa4b437c7d57f10316b77
Aaron3007-wq <22amclynn.acc@lwetb.ie>
2025-09-04 08:52:53 +0100

    Add files via upload

---
+++ BQ174-6.py
@@ -0,0 +1,8 @@
+#Author:Aaron Mclynn
+#Date: 2/9/25
+#Desc:Radius A python program that accepts radius and prints area.
+radius = input('whats the radius of a circle')
+radius = int(radius)
+total = 3.14159 * (radius * radius)
+print('the area is', total,)
+
---
+++ BQ174-7.py
@@ -0,0 +1,20 @@
+#Author:Aaron Mclynn
+#Date: 2/9/25
+#Desc: Marks in 5 subjects and outputs average marks
+number1 = input('what are your marks for subject 1')
+number2 = input('what are your marks for subject 2')
+number3 = input('what are your marks for subject 3')
+number4 = input('what are your marks for subject 4')
+number5 = input('what are your marks for subject 5')
+
+number1 = int(number1)
+number2 = int(number2)
+number3 = int(number3)
+number4 = int(number4)
+number5 = int(number5)
+
+total = (number1 + number2 + number3 + number4 + number5) /5
+print(total)
+#i did not understnad Q8-11
+
+
