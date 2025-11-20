7a142bd19913cd49e53ca545b61452cbc7d40a09
Aaron3007-wq <22amclynn.acc@lwetb.ie>
2025-11-20 08:49:00 +0000

    Add files via upload

---
+++ Ex10 Q2 pt2.py
@@ -0,0 +1,9 @@
+#Author:Aaron Mclynn
+#Date:3/11/25
+#Desc:Ex10 Q2 pt2.calculates the length of a string using a while loop
+total = 0
+N = input('Enter word')
+while total<len(N):
+    total = total + 1
+    print (total)
+    
---
+++ Ex10 Q2.py
@@ -0,0 +1,8 @@
+#Author:Aaron Mclynn
+#Date:3/11/25
+#Desc:Ex10 Q2, Calculate the length of a string using a for loop.
+total = 0
+N = input ('Enter word')
+for i in N:
+    total = total + 1
+    print(total)
