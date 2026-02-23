c1f45446e4b3e290e17785f1c0a5c481b5a0616d
Aaron3007-wq <22amclynn.acc@lwetb.ie>
2026-02-23 12:58:55 +0000

    Add files via upload

---
+++ Question (i).py
@@ -0,0 +1,10 @@
+#Author:Aaron Mclynn
+#Date:23/2/26
+#Desc:Tuple Excercise:Python Maths Expressions Q1:(i)
+import math
+total = 0
+a = 3
+b = 5
+c = 7
+total = math.sqrt(a) + math.sqrt(b) + math.sqrt(c)
+print(total)
---
+++ Question (ii).py
@@ -0,0 +1,9 @@
+#Author:Aaron Mclynn
+#Date:23/2/26
+#Desc:Tuple Excercise:Python Maths Expressions Q1:(ii)
+import math
+total = 0
+y = 4
+e = 7
+total = 2 - math.exp(y) + math.exp(e) + 4*y
+print(total)
