a275d2b949a0c601ff05fe624f4588e082dbe937
Aaron3007-wq <22amclynn.acc@lwetb.ie>
2026-09-03 10:43:34 +0100

    Add files via upload

---
+++ Binary Search Question 1.py
@@ -0,0 +1,20 @@
+#Author:Aaron Mclynn
+#Date:3/9/26
+#Desc:Binary Search
+lst = [2,5,8,12,16,56,23,72,38,91]
+list.sort(lst)
+Target_Value = 23
+
+High_index = len(lst)
+High_index = High_index - 1
+
+Low_index = 0
+
+while Low_index <= High_index:
+    cal = 
+    
+
+
+
+print(lst)
+print(High_index)
