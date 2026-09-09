f41602ad9498e31925c8a8e1e8e41d590cf95372
Aaron3007-wq <22amclynn.acc@lwetb.ie>
2026-09-09 10:44:17 +0100

    Add files via upload

---
+++ Selection sort question 1.py
@@ -0,0 +1,15 @@
+#Author:Aaron Mclynn
+#Date:9/9/26
+#Desc:Selection sort
+lst = [2,4,5]
+
+
+for i in range (0,len(lst)-1):
+    i == lst[0]
+    for j in range (i+1(len(lst)-1)):
+        if j < i:
+            j == i
+        else:
+            i == lst[0]
+        print(lst)
+        
